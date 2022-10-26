6b2936e1a854ce093c4bd73cee6bb4e97b599a0e
nmskm240 <bokaronogod@gmail.com>
2022-10-27 00:01:16 +0900

    read to spreadsheet

--- src/main.py
+++ src/main.py
@@ -1,69 +1,18 @@
-from __future__ import print_function
+import os
+import gspread
+from google.oauth2.service_account import Credentials
+from dotenv import load_dotenv
+load_dotenv()
 
-import os.path
+SCOPES = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
+]
 
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+credentials = Credentials.from_service_account_file(
+    "./token.json", scopes=SCOPES)
+client = gspread.authorize(credentials)
 
-from google.cloud import firestore
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1wGNxZeoGaBMyUlSlWK0u0Ko8bdnNtx7rksA5qy_k-BQ'
-SAMPLE_RANGE_NAME = 'フル!B9:C66'
-
-
-def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                './credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    db = firestore.Client()
-
-    try:
-        service = build('sheets', 'v4', credentials=creds)
-
-        # Call the Sheets API
-        sheet = service.spreadsheets()
-        print(sheet)
-        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                    range=SAMPLE_RANGE_NAME).execute()
-        values = result.get('values', [])
-
-        if not values:
-            print('No data found.')
-            return
-
-        print('Name, Major:')
-        doc_ref = db.collection('members').get()
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s' % (row[0]))
-    except HttpError as err:
-        print(err)
-
-
-if __name__ == '__main__':
-    main()
+spreadsheet = client.open_by_key(os.environ["NAMELIST_SHEET_ID"])
+cells = spreadsheet.named_range("MemberData")
+print(cells)
