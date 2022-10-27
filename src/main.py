5b2580d5984d6d558d8708154f9b5dd647906eaa
nmskm240 <bokaronogod@gmail.com>
2022-10-27 09:48:52 +0900

    introduction of flask

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os
 import gspread
 from google.oauth2.service_account import Credentials
+from flask import Flask
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -9,10 +10,15 @@ SCOPES = [
     'https://www.googleapis.com/auth/drive'
 ]
 
+app = Flask(__name__)
 credentials = Credentials.from_service_account_file(
     "./token.json", scopes=SCOPES)
 client = gspread.authorize(credentials)
 
-spreadsheet = client.open_by_key(os.environ["NAMELIST_SHEET_ID"])
-cells = spreadsheet.named_range("MemberData")
-print(cells)
+@app.route("/")
+def index():
+    spreadsheet = client.open_by_key(os.environ["NAMELIST_SHEET_ID"])
+    cells = spreadsheet.named_range("MemberData")
+    return cells[1].value
+
+app.run(host="127.0.0.1", port=5000)
